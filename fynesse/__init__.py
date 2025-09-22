85b42375de738c16ac7706d9030b293a963b6517
DÉLL <chiri.levisk@gmail.com>
2025-09-22 15:46:00 +0300

    added function in init

--- fynesse/__init__.py
+++ fynesse/__init__.py
@@ -1,3 +1,4 @@
+# Import functions from the package
 from fynesse.access import attendance_acs, population_acs
 from fynesse.assess import attendance_ass, population_ass
 from fynesse.address import attendance_add, population_add
