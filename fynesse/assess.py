1504638b329ca2f2864ca75c21c4cc5fea2424d3
DÉLL <chiri.levisk@gmail.com>
2025-09-22 15:08:11 +0300

    start acs,ass,add

--- fynesse/assess.py
+++ fynesse/assess.py
@@ -1,48 +1,24 @@
 import pandas as pd
-from typing import List, Tuple, Optional
 
+# Global counties list
+counties_list = [
+    "BARINGO","BOMET","BUNGOMA","BUSIA","ELGEYO-MARAKWET","EMBU","GARISSA",
+    "HOMA BAY","ISIOLO","KAJIADO","KAKAMEGA","KERICHO","KIAMBU","KILIFI",
+    "KIRINYAGA","KISII","KISUMU","KITUI","KWALE","LAIKIPIA","LAMU","MACHAKOS",
+    "MAKUENI","MANDERA","MARSABIT","MERU","MIGORI","MOMBASA","MURANGA",
+    "NAIROBI","NAKURU","NANDI","NAROK","NYAMIRA","NYANDARUA","NYERI",
+    "SAMBURU","SIAYA","TAITA-TAVETA","TANA RIVER","THARAKA-NITHI","TRANS NZOIA",
+    "TURKANA","UASIN GISHU","VIHIGA","WAJIR","WEST POKOT"
+]
 
-class DataAssessment:
-    def __init__(self, data_access):
-        self.data_access = data_access
-        self.default_features = [
-            ("building", None),
-            ("amenity", None),
-            ("amenity", "school"),
-            ("amenity", "hospital"),
-            ("amenity", "restaurant"),
-            ("amenity", "cafe"),
-            ("shop", None),
-            ("tourism", None),
-            ("tourism", "hotel"),
-            ("tourism", "museum"),
-            ("leisure", None),
-            ("leisure", "park"),
-            ("historic", None),
-            ("amenity", "place_of_worship"),
-        ]
-
-    def assess_poi_distribution(self, features: Optional[List[Tuple]] = None):
-        if self.data_access.pois is None:
-            self.data_access.access_pois()
-        if features is None:
-            features = self.default_features
-            
-        pois_df = pd.DataFrame(self.data_access.pois)
-        pois_df['latitude'] = pois_df.apply(lambda row: row.geometry.centroid.y, axis=1)
-        pois_df['longitude'] = pois_df.apply(lambda row: row.geometry.centroid.x, axis=1)
-        
-        poi_counts = {}
-        for key, value in features:
-            if key in pois_df.columns:
-                if value:
-                    poi_counts[f"{key}:{value}"] = (pois_df[key] == value).sum()
-                else:
-                    poi_counts[key] = pois_df[key].notnull().sum()
-            else:
-                poi_counts[f"{key}:{value}" if value else key] = 0
-        
-        results = pd.DataFrame(list(poi_counts.items()), columns=["POI Type", "Count"])
-        print("POI Assessment Summary:")
-        print(results)
-        return results
+def attendance_ass(df: pd.DataFrame, counties: list = counties_list) -> pd.DataFrame:
+    """
+    Filter for counties, clean numeric values, and sort alphabetically.
+    """
+    area_col, school_col = df.columns[0], df.columns[2]
+    df_county = df[df[area_col].str.upper().isin([c.upper() for c in counties])].drop_duplicates(subset=[area_col]).copy()
+    df_county[school_col] = pd.to_numeric(
+        df_county[school_col].astype(str).str.replace('[,""]','', regex=True),
+        errors='coerce'
+    )
+    return df_county.sort_values(by=area_col).reset_index(drop=True), area_col, school_col
