b1186ea9c4d6927a866be7d653291b413c7fd796
DÉLL <chiri.levisk@gmail.com>
2025-09-23 15:16:32 +0300

    remove correlation_ass

--- fynesse/__init__.py
+++ fynesse/__init__.py
@@ -1,3 +1,3 @@
 from fynesse.access import attendance_acs, population_acs, education_acs, schools_acs, secondary_acs, univ_tvet_acs
-from fynesse.assess import attendance_ass, population_ass, education_ass, schools_ass, secondary_ass, univ_tvet_ass, correlation_ass, combine_correlation_data, prepare_regression_data, run_tvet_regression, regression2_ass, cluster_ass
+from fynesse.assess import attendance_ass, population_ass, education_ass, schools_ass, secondary_ass, univ_tvet_ass, combine_correlation_data, prepare_regression_data, run_tvet_regression, regression2_ass, cluster_ass
 from fynesse.address import attendance_add, population_add, population_vs_attendance, education_add, schools_add, secondary_add, univ_tvet_add, correlation_add, regression_add, tvet_regression_add, regression2_add, cluster_add
