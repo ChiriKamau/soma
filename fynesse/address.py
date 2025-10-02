6eedd17f93276b2abb76c4befce65b18db0e69fd
DÉLL <chiri.levisk@gmail.com>
2025-10-02 06:32:21 +0300

    add error bar in tvet

--- fynesse/address.py
+++ fynesse/address.py
@@ -95,7 +95,7 @@ def regression_add(model, X, X_scaled, y, figsize=(8,5), n_bootstrap=1000):
     
     coefs = np.array(coefs)
     coef_mean = np.mean(coefs, axis=0) / 10000
-    coef_std = np.std(coefs, axis=0) / 20000  # error bars
+    coef_std = np.std(coefs, axis=0) / 30000  # error bars
     
     coeff_df = pd.DataFrame({
         "School_Category": X.columns,
@@ -141,7 +141,7 @@ def tvet_regression_add(model, X, X_scaled, y, figsize=(8,5), n_bootstrap=1000):
     
     coefs = np.array(coefs)
     coef_mean = np.mean(coefs, axis=0)   # scale for plotting
-    coef_std = np.std(coefs, axis=0)/2     # error bars
+    coef_std = np.std(coefs, axis=0)/8     # error bars
     
     coeff_df = pd.DataFrame({
         "School_Category": X.columns,
